df1fb22495a51010f9303f1a35671e1d2cc78d83
id github: wizabela <izabela.wyrebska@gmail.com>
2022-05-17 18:24:28 +0200

    Add Map, Btn and Header components

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Map.css'
+
+export const Map = () => {
+    return (
+        <div className="map"/>
+    )
+};
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Btn} from "../common/Btn";
+import './Header.css';
+
+export const Header = ()=>(
+    <header>
+        <h1>
+            <strong>Mega</strong> Ogłoszenia
+        </h1>
+        <Btn text="Dodaj Ogłoszenie"/>
+        <div className="search">
+            <input type="text"/><Btn text="Szukaj"/>
+        </div>
+    </header>
+)
