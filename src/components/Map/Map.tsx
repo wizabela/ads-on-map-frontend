fac7afc10d97b441e6f23a55740d393bdb15b2aa
id github: wizabela <izabela.wyrebska@gmail.com>
2022-05-18 20:05:00 +0200

    add styles for markers, popups and icons

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icons';
+
+import 'leaflet/dist/leaflet.css'
 import './Map.css'
 
 export const Map = () => {
     return (
-        <div className="map"/>
+        <div className="map">
+            <MapContainer center={[52.2012031, 20.9614427]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker position={[52.2012031, 20.9614427]}>
+                    <Popup>
+                        <h2>Park Szczęśliwicki</h2>
+                        <p>Super park na spacery</p>
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     )
 };
